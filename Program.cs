aec0d8aaf291313c298068ffdb560d3645f97e74
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2022-10-24 12:45:22 -0700

    Update 4.0

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using SharpOSC;
 using VRCOSCUtils;
 using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Timers;
+using Timer = System.Threading.Timer;
 
 namespace VRCOSCUtils
 {
@@ -132,7 +137,7 @@ namespace VRCOSCUtils
             CurrentSound = SoundPadProc.MainWindowTitle;
             if (CurrentSound == "Soundpad")
             {
-                return "Idling on SoundPad";
+                return $"Idling on SoundPad || CPU: {Math.Round(getCurrentCpuUsage())}% || RAM: {(Math.Round((capacity - available) / capacity * 100, 0)).ToString()}%  || GPU: { Math.Round(GetGPUUsage())}%";
             }
             return $"{SoundPadProc.MainWindowTitle} || CPU: {Math.Round(getCurrentCpuUsage())}% || RAM: {(Math.Round((capacity - available) / capacity * 100, 0)).ToString()}%  || GPU: { Math.Round(GetGPUUsage())}%";
         }
@@ -161,6 +166,129 @@ namespace VRCOSCUtils
 
             }
         }
+        #region https://github.com/NYAN-x-CAT/LimeLogger/blob/master/LimeLogger/LimeLogger.cs 
+
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                return SetWindowsHookEx(WHKEYBOARDLL, proc, GetModuleHandle(curProcess.ProcessName), 0);
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                bool capsLock = (GetKeyState(0x14) & 0xffff) != 0;
+                bool shiftPress = (GetKeyState(0xA0) & 0x8000) != 0 || (GetKeyState(0xA1) & 0x8000) != 0;
+                string currentKey = KeyboardLayout((uint)vkCode);
+
+              
+
+                if (((Keys)vkCode).ToString() == "Space" && t.Enabled == false)
+                {
+                    NumSpace += 1;
+                    SpacebarPS += 1;
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        public static System.Timers.Timer t = new System.Timers.Timer();
+
+        public static void Joe()
+        {
+
+        }
+        public static void Send(Object source)
+        {
+            LogUtils.Log($"Spacebar Counter: {NumSpace} || Spacebar Per Second: {SpacebarPS}");
+            oscSender.Send(new OscMessage("/chatbox/input", $"Spacebar Counter: {NumSpace} || Spacebar Per Second: {SpacebarPS}", true, true));
+
+            SpacebarPS = 0;
+
+
+
+        }
+        public static int NumSpace = 0;
+        private static string KeyboardLayout(uint vkCode)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                byte[] vkBuffer = new byte[256];
+                if (!GetKeyboardState(vkBuffer)) return "";
+                uint scanCode = MapVirtualKey(vkCode, 0);
+                IntPtr keyboardLayout = GetKeyboardLayout(GetWindowThreadProcessId(GetForegroundWindow(), out uint processId));
+                ToUnicodeEx(vkCode, scanCode, vkBuffer, sb, 5, 0, keyboardLayout);
+                return sb.ToString();
+            }
+            catch { }
+            return ((Keys)vkCode).ToString();
+        }
+
+        private static string GetActiveWindowTitle()
+        {
+            try
+            {
+                IntPtr hwnd = GetForegroundWindow();
+                GetWindowThreadProcessId(hwnd, out uint pid);
+                Process p = Process.GetProcessById((int)pid);
+                string title = p.MainWindowTitle;
+                if (string.IsNullOrWhiteSpace(title))
+                    title = p.ProcessName;
+                return title;
+            }
+            catch (Exception)
+            {
+                return "???";
+            }
+        }
+
+
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        private static int WHKEYBOARDLL = 13;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetKeyboardLayout(uint idThread);
+
+        [DllImport("user32.dll")]
+        static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+        #endregion
         public static Task UpdateOSCSP()
         {
             while (true)
@@ -172,6 +300,7 @@ namespace VRCOSCUtils
 
             }
         }
+        public static int SpacebarPS = 0;
         public static Task ClanTagChanger(string CustomText)
         {
             CustomText += " ";
@@ -183,13 +312,14 @@ namespace VRCOSCUtils
                 {
                     try
                     {
+
                         currentindex += 1;
                         CurrentString += cha;
                         Console.WriteLine(CurrentString);
 
                         oscSender.Send(new OscMessage("/chatbox/input", CurrentString, true, true));
 
-                        Thread.Sleep(2000);
+                        Thread.Sleep(1500);
                         if (cha == ' ')
                         {
                             currentindex -= 1;
@@ -205,11 +335,12 @@ namespace VRCOSCUtils
                 {
                     try
                     {
+
                         var s = CurrentString.Remove(currentindex);
                         Console.WriteLine(s);
                         oscSender.Send(new OscMessage("/chatbox/input", s, true, true));
                         currentindex -= 1;
-                        Thread.Sleep(2000);
+                        Thread.Sleep(1500);
 
                     }
                     catch (Exception e)
@@ -232,16 +363,89 @@ namespace VRCOSCUtils
                 File.WriteAllText(Environment.CurrentDirectory + "\\CustomName.txt", "VRCOSCSPOTIFY");
             }
         }
+        public static Task Aimware()
+        {
+            string[] Aimware = new string[] { "---AIMWARE.NET---", "----AIMWARE.NET--", "-----AIMWARE.NET-", "----AIMWARE.NET", "T------AIMWARE.NE", "ET------AIMWARE.N", "NET------AIMWARE.", ".NET------AIMWARE", "E.NET------AIMWAR", "RE.NET------AIMWA", "ARE.NET------AIMW", "WARE.NET------AIM", "MWARE.NET------AI", "IMWARE.NET------A", "AIMWARE.NET------", "-AIMWARE.NET-----", "--AIMWARE.NET----", "---AIMWARE.NET---" };
+            while (true)
+            {
+                foreach (var strin in Aimware)
+                {
+                    LogUtils.Log(strin);
+                    oscSender.Send(new OscMessage("/chatbox/input", strin, true, true));
+                    Thread.Sleep(1500);
+                }
+            }
+        }
+        public static Task NeverLose()
+        {
+            string[] Neverlose = new string[] { " | ", @" |\\ ", @" |\\| ", @" N ", @" N3 ", @" Ne ", @" Ne\\ ", @" Ne\\/ ", @" Nev ", @" Nev3 ", @" Neve ", @" Neve| ", @" Neve|2 ", @" Neve|_ ", @" Nevel ", @" Nevel0 ", @" Nevelo ", @" Nevelo5 ", @" Nevelos ", @" Nevelos3 ", @" Nevelose " };
+            while (true)
+            {
+                for (int i = 0; i < Neverlose.Length; i++)
+                {
+                    LogUtils.Log(Neverlose[i]);
+
+                    oscSender.Send(new OscMessage("/chatbox/input", Neverlose[i], true, true));
+                    Thread.Sleep(1500);
+
+
+                }
+
+
+
+            }
+        }
+        public static Task Gamesense()
+        {
+            string[] gamesense = new string[] { "g", "ga", "gam", "game", "games", "gamese", "gamesen", "gamesens", "gamesense" };
+            while (true)
+            {
+                for (int i = 0; i < gamesense.Length; i++)
+                {
+                    LogUtils.Log(gamesense[i]);
+
+                    oscSender.Send(new OscMessage("/chatbox/input", gamesense[i], true, true));
+                    Thread.Sleep(1500);
+                }
+            }
+        }
+        
+        public static void Misc()
+        {
+            LogUtils.Clear();
+            LogUtils.Error("Please enter valid input\n");
+
+            LogUtils.Log("Options:\n1. Aimware Clantag || 2. NeverLose Clantag || 3. gamesense Clantag");
+            var s = Console.ReadLine();
+            switch (s)
+            {
+
+                case "1":
+                    Aimware();
+                    break;
+                case "2":
+                    NeverLose();
+                    break;
+                case "3":
+                    Gamesense();
+                    break;
+                default:
+
+                    Misc();
+                    break;
+            }
+        }
         public static void Redo()
         {
 
             LogUtils.Clear();
             LogUtils.Error("Please enter valid input\n");
 
-            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats");
+            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats \n|| 4. Misc || 5. SpaceBar Counter");
             var s = Console.ReadLine();
             switch (s)
             {
+                
                 case "1":
                     UpdateOSC();
                     break;
@@ -250,6 +454,13 @@ namespace VRCOSCUtils
                     break;
                 case "3":
                     UpdateOSCSP();
+                    break;
+                case "4":
+                    Misc();
+                    break;
+                case "5":
+                    _hookID = SetHook(_proc);
+                    Joe();
                     break;
                 default:
 
@@ -262,7 +473,7 @@ namespace VRCOSCUtils
             oscSender = new UDPSender("127.0.0.1", 9000);
             Init();
             LogUtils.Logo();
-            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats");
+            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats \n|| 4. Misc || 5. SpaceBar Counter");
             var s = Console.ReadLine();
             switch (s)
             {
@@ -275,7 +486,16 @@ namespace VRCOSCUtils
                 case "3":
                     UpdateOSCSP();
                     break;
+                case "4":
+                    Misc();
+                    break;
+                case "5":
+                    _hookID = SetHook(_proc);
+                    Timer t = new Timer(Send, null, 0, 1500);
 
+                    Application.Run();
+                    
+                    break;
                 default:
                     Redo();
                     break;
