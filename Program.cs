1a51e6e6f873983d005394a0407d982d2e186686
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2022-12-10 16:30:47 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -64,6 +64,7 @@ namespace VRCOSCUtils
             }
         }
         public static UDPSender oscSender;
+        public static UDPDuplex oscListener;
 
         public static string GetSpotifySong()
         {
@@ -148,13 +149,22 @@ namespace VRCOSCUtils
             Thread.Sleep(1000);
             return (dynamic)cpuCounter.NextValue();
         }
-        public static float s;
+
         public static string CurrentSongCheck = null;
 
         public static string CurrentSong = null;
 
         public static string CurrentSound = null;
 
+        
+
+        public static string GetElapsed()
+        {
+            TimeSpan WatchTimeSpan = Watch.Elapsed;
+            
+            return String.Format("{0:00}:{1:00}:{2:00}",
+            WatchTimeSpan.Hours, WatchTimeSpan.Minutes, WatchTimeSpan.Seconds); 
+        }
         public static Task UpdateOSC()
         {
             while (true)
@@ -204,8 +214,8 @@ namespace VRCOSCUtils
         }
         public static void Send(Object source)
         {
-            LogUtils.Log($"Spacebar Counter: {NumSpace} || Spacebar Per Second: {SpacebarPS}");
-            oscSender.Send(new OscMessage("/chatbox/input", $"Spacebar Counter: {NumSpace} || Spacebar Per Second: {SpacebarPS}", true, true));
+            LogUtils.Log($"Spacebar Counter: {NumSpace} || Spacebar Per Second: {SpacebarPS} || Elapsed: {GetElapsed()}");
+            oscSender.Send(new OscMessage("/chatbox/input", $"Spacebar Counter: {NumSpace} || Spacebar Per Second: {SpacebarPS} || Elapsed: {GetElapsed()}" , true, true));
 
             SpacebarPS = 0;
 
@@ -435,13 +445,29 @@ namespace VRCOSCUtils
                     break;
             }
         }
+        public static void OnReceive(OscPacket bytes)
+        {
+           // Console.WriteLine($"{((OscMessage)bytes).Address} {string.Join(" ", ((OscMessage)bytes).Arguments)}" );
+           if (((OscMessage)bytes).Address.ToString() == "/avatar/parameters/Upright")
+           {
+                if (string.Join(" ", ((OscMessage)bytes).Arguments).Contains("1"))
+                {
+                    Console.WriteLine("Crouching");
+                }
+                else
+                {
+                    Console.WriteLine("Standing");
+
+                }
+            }
+        }
         public static void Redo()
         {
 
             LogUtils.Clear();
             LogUtils.Error("Please enter valid input\n");
 
-            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats \n|| 4. Misc || 5. SpaceBar Counter");
+            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats \n|| 4. Misc || 5. SpaceBar Counter || 6. KeyFramed Text");
             var s = Console.ReadLine();
             switch (s)
             {
@@ -462,18 +488,113 @@ namespace VRCOSCUtils
                     _hookID = SetHook(_proc);
                     Joe();
                     break;
+                case "6":
+                    KeyFrameInput();
+                    break;  
                 default:
 
                     Redo();
                     break;
             }
         }
+        public static List <string> joe;
+        public static int FrameCount;
+        public static void SendFrames()
+        {
+            while (true)
+            {
+                for (int i = 0; i < joe.Count; i++)
+                {
+                    LogUtils.Log(joe[i]);
+                    oscSender.Send(new OscMessage("/chatbox/input", joe[i], true, true));
+                    Thread.Sleep(1500);
+                }
+            }
+        }
+        public static void KeyFrameInput()
+        {
+            joe = new List<string>();
+            if (!string.IsNullOrEmpty(File.ReadAllText($"{Environment.CurrentDirectory}\\KeyFrameConfig.txt")))
+            {
+                LogUtils.Log("Do you want to load your previous frames config?\ny for yes || n for no");
+                var userinput4 = Console.ReadLine();
+                if (userinput4 == "y")
+                {
+                    var filestring = File.ReadAllText($"{Environment.CurrentDirectory}\\KeyFrameConfig.txt").Split(',');
+                    foreach (var file in filestring)
+                    {
+                        joe.Add(file);
+                    }
+                    SendFrames();
+
+                }
+                else
+                {
+                    LogUtils.Log("Enter frame count:");
+                    var userinput = Console.ReadLine();
+                    FrameCount = int.Parse(userinput);
+
+                    for (int i = 0; i < FrameCount; i++)
+                    {
+                        LogUtils.Log($"Enter frame {i} value:");
+                        var userinput2 = Console.ReadLine();
+                        joe.Add(userinput2);
+                    }
+                    LogUtils.Log("Would you like to save your frames to config?\ny for yes || n for no");
+                    var userinput3 = Console.ReadLine();
+
+                    if (userinput3 == "y")
+                    {
+                        var filelist = new StringBuilder();
+                        for (int i = 0; i < joe.Count; i++)
+                        {
+                            filelist.Append(joe[i] + ",");
+                        }
+                        File.WriteAllText($"{Environment.CurrentDirectory}\\KeyFrameConfig.txt", filelist.ToString());
+                    }
+
+                    SendFrames();
+                }
+            }
+            else
+            {
+                LogUtils.Log("Enter frame count:");
+                var userinput = Console.ReadLine();
+                FrameCount = int.Parse(userinput);
+
+                for (int i = 0; i < FrameCount; i++)
+                {
+                    LogUtils.Log($"Enter frame {i} value:");
+                    var userinput2 = Console.ReadLine();
+                    joe.Add(userinput2);
+                }
+                LogUtils.Log("Would you like to save your frames to config?\ny for yes || n for no");
+                var userinput3 = Console.ReadLine();
+
+                if (userinput3 == "y")
+                {
+                    var filelist = new StringBuilder();
+                    for (int i = 0; i < joe.Count; i++)
+                    {
+                        filelist.Append(joe[i] + ",");
+                    }
+                    File.WriteAllText($"{Environment.CurrentDirectory}\\KeyFrameConfig.txt", filelist.ToString());
+                }
+
+                SendFrames();
+            }
+          
+        
+        }
+        public static Stopwatch Watch;
         static void Main(string[] args)
         {
+            //oscListener = new UDPDuplex("127.0.0.1", 9000, 9001, OnReceive );
+           // Console.ReadKey();
             oscSender = new UDPSender("127.0.0.1", 9000);
             Init();
             LogUtils.Logo();
-            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats \n|| 4. Misc || 5. SpaceBar Counter");
+            LogUtils.Log("Options:\n1. Spotify and PC Stats || 2. Animated ClanTag (Must Edit CustomName.txt) || 3. Display Soundpad and PC Stats \n|| 4. Misc || 5. SpaceBar Counter || 6. KeyFramed Text");
             var s = Console.ReadLine();
             switch (s)
             {
@@ -493,8 +614,13 @@ namespace VRCOSCUtils
                     _hookID = SetHook(_proc);
                     Timer t = new Timer(Send, null, 0, 1500);
 
+                    Watch = Stopwatch.StartNew();
+                    Watch.Start();
                     Application.Run();
                     
+                    break;
+                case "6":
+                    KeyFrameInput();
                     break;
                 default:
                     Redo();
