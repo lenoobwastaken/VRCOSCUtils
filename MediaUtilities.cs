27fb8b833cd3aab0b45fee57af898faa04d31552
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2023-01-22 16:51:13 -0800

    Add files via upload

--- MediaUtilities.cs
+++ MediaUtilities.cs
@@ -11,6 +11,8 @@ using System.Management;
 using Windows.Media.Control;
 using Windows.Media.Core;
 using CoreOSC;
+using Windows.ApplicationModel.Store;
+using Microsoft.Diagnostics.Tracing.Session;
 //using SDL2;
 namespace VRCOSCUtils
 {
@@ -170,7 +172,65 @@ namespace VRCOSCUtils
             mediaManager.OnAnyMediaPropertyChanged += MediaManager_OnAnyMediaPropertyChanged;
 
             mediaManager.Start();
+
 #if !DEBUG
+
+       
+            m_EtwSession = new TraceEventSession("mysess");
+            m_EtwSession.StopOnDispose = true;
+            m_EtwSession.EnableProvider("Microsoft-Windows-D3D9");
+            m_EtwSession.EnableProvider("Microsoft-Windows-DXGI");
+
+            //handle event
+            m_EtwSession.Source.AllEvents += data =>
+            {
+                //filter out frame presentation events
+                if (((int)data.ID == EventID_D3D9PresentStart && data.ProviderGuid == D3D9_provider) ||
+                ((int)data.ID == EventID_DxgiPresentStart && data.ProviderGuid == DXGI_provider))
+                {
+                    int pid = data.ProcessID;
+                    long t;
+
+                    lock (sync)
+                    {
+                        t = watch.ElapsedMilliseconds;
+
+                        //if process is not yet in Dictionary, add it
+                        if (!frames.ContainsKey(pid))
+                        {
+                            frames[pid] = new TimestampCollection();
+
+                            string name = "";
+                            var proc = Process.GetProcessById(pid);
+                            if (proc != null)
+                            {
+                                using (proc)
+                                {
+                                    name = proc.ProcessName;
+                                }
+                            }
+                            else name = pid.ToString();
+
+                            frames[pid].Name = name;
+                        }
+
+                        //store frame timestamp in collection
+                        frames[pid].Add(t);
+                    }
+                }
+            };
+
+            watch = new Stopwatch();
+            watch.Start();
+
+            Thread thETW = new Thread(EtwThreadProc);
+            thETW.IsBackground = true;
+            thETW.Start();
+
+            // Thread thOutput = new Thread(OutputThreadProc);
+            //  thOutput.IsBackground = true;
+            //  thOutput.Start();
+
             switch (type)
             {
                 case MediaType.Spotify:
@@ -178,17 +238,38 @@ namespace VRCOSCUtils
 
                     break;
                 case MediaType.Soundpad:
+                    SendSoundpad();
                     break;
                 default:
                     Console.WriteLine("This should not happen");
                     break;
             }
             Console.ReadLine();
+            m_EtwSession.Dispose();
+
 #endif
 #if DEBUG
         
 #endif
         }
+        public const int EventID_D3D9PresentStart = 1;
+        public const int EventID_DxgiPresentStart = 42;
+
+        //ETW provider codes
+        public static readonly Guid DXGI_provider = Guid.Parse("{CA11C036-0102-4A2D-A6AD-F03CFED5D3C9}");
+        public static readonly Guid D3D9_provider = Guid.Parse("{783ACA0A-790E-4D7F-8451-AA850511C6B9}");
+
+        static TraceEventSession m_EtwSession;
+        static Dictionary<int, TimestampCollection> frames = new Dictionary<int, TimestampCollection>();
+        static Stopwatch watch = null;
+        static object sync = new object();
+
+        static void EtwThreadProc()
+        {
+            //start tracing
+            m_EtwSession.Source.Process();
+        }
+        public static string currentfps = null;
         public static bool NoStats = false;
         public static void SendSpotify()
         {
@@ -197,15 +278,74 @@ namespace VRCOSCUtils
                 if (NoStats == true)
 
                 {
-                    Program.oscSender.Send(new OscMessage("/chatbox/input", $"{SpotifyInfo()}", true, true));
-                    LogUtils.Log("Sent!");
+                    long t1, t2;
+                    long dt = 2000;
+                    //  Console.Clear();
+                    //   Console.WriteLine(DateTime.Now.ToString() + "." + DateTime.Now.Millisecond.ToString());
+                    // Console.WriteLine();
 
-                    Thread.Sleep(1500);
+                    lock (sync)
+                    {
+                        t2 = watch.ElapsedMilliseconds;
+                        t1 = t2 - dt;
+
+                        foreach (var x in frames.Values)
+                        {
+                            if (x.Name == "VRChat")
+                            {
+                                //Console.Write("VRChat: ");
+                                //get the number of frames
+                                int count = x.QueryCount(t1, t2);
+
+                                //calculate FPS
+                                //  Console.WriteLine("{0} FPS", (double)count / dt * 1000.0);
+                                currentfps = $"{Math.Round((double)count / dt * 1000.0)}";
+                                Program.oscSender.Send(new OscMessage("/chatbox/input", $"{SpotifyInfo()} || FPS: {currentfps}", true, true));
+                                LogUtils.Log("Sent!");
+                                //  Console.WriteLine(currentfps);
+                            }
+
+
+                        }
+                    }
+                  //  Program.oscSender.Send(new OscMessage("/chatbox/input", $"{SpotifyInfo()} || FPS: {FPSUtils.currentfps}", true, true));
+                 //   LogUtils.Log("Sent!");
+
+                    Thread.Sleep(1600);
                 }
                 else
                 {
-                    Program.oscSender.Send(new OscMessage("/chatbox/input", $"{SpotifyInfo()} || CPU: {GetCPU()} || RAM: {GetRAM()} || GPU: {GetGPU()}", true, true));
-                    LogUtils.Log("Sent!");
+                    long t1, t2;
+                    long dt = 2000;
+                    //  Console.Clear();
+                    //   Console.WriteLine(DateTime.Now.ToString() + "." + DateTime.Now.Millisecond.ToString());
+                    // Console.WriteLine();
+
+                    lock (sync)
+                    {
+                        t2 = watch.ElapsedMilliseconds;
+                        t1 = t2 - dt;
+
+                        foreach (var x in frames.Values)
+                        {
+                            if (x.Name == "VRChat")
+                            {
+                                //Console.Write("VRChat: ");
+                                //get the number of frames
+                                int count = x.QueryCount(t1, t2);
+
+                                //calculate FPS
+                                //  Console.WriteLine("{0} FPS", (double)count / dt * 1000.0);
+                                currentfps = $"{Math.Round((double)count / dt * 1000.0)}";
+                                //  Console.WriteLine(currentfps);
+                                Program.oscSender.Send(new OscMessage("/chatbox/input", $"{SpotifyInfo()} || CPU: {GetCPU()} || RAM: {GetRAM()} || GPU: {GetGPU()} || FPS: {currentfps}", true, true));
+                                LogUtils.Log("Sent!");
+
+                            }
+
+
+                        }
+                    }
                 }
                 //Thread.Sleep(1500);
             }
